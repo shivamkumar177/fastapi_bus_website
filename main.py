942b4e0a7e76a995a18ccc93a4f25faedd24bb6d
SHIVAM KUMAR <shivamk1717@gmail.com>
2022-11-07 13:22:51 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from app.routers import booking
+from app.routers import booking, authentication, admin
 
 app = FastAPI()
 
 app.include_router(booking.router)
+app.include_router(authentication.router)
+app.include_router(admin.router)
